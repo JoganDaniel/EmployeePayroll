0041a995236450717757850fcb95617721b85ad6
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-04 11:32:51 +0530

    [jogan] Add : stored procedure

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -9,8 +9,29 @@ internal class Program
         //Payroll_Service_Operation.InsertDatebase();
         //Payroll_Service_Operation.ReadFromDatabase();
         //Payroll_Service_Operation.RetriveBetweenDatebase();
-        string query1 = "update employee_payroll set Gender = 'M' where id in (1,4);\r\nupdate employee_payroll set Gender = 'F' where id between 2 and 3;";
-        Payroll_Service_Operation.Queryexecte(query1);
+        //string query1 = "update employee_payroll set Gender = 'M' where id in (1,4);\r\nupdate employee_payroll set Gender = 'F' where id between 2 and 3;";
+        //Payroll_Service_Operation.Queryexecte(query1);
+        Employee employee = new Employee()
+        {
+            Name = "a",
+            City = "b",
+            Address = "c"
+
+        };
+
+        Employee employee1 = new Employee()
+        {
+            EmpId = 3,
+            Name = "updatedName",
+            City = "UpdatedCity",
+            Address = "UpdatedAddress"
+
+        };
+        EmployeeOperation operation = new EmployeeOperation();
+        operation.AddEmployee(employee);
+        operation.exuctedorNot(operation.AddEmployee(employee));
+        operation.exuctedorNot(operation.UpdateEmployee(employee1));
+        operation.DisplayAllData();
     }
 
 }
